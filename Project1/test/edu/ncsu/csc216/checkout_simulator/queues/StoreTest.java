bb0faa69fe412ffb3ca4c5c301da4d6af54b3734
Brian Morris <bcmorri3@ncsu.edu>
2017-02-28 21:39:19 -0500

    Added StoreTest.

---
+++ Project1/test/edu/ncsu/csc216/checkout_simulator/queues/StoreTest.java
@@ -0,0 +1,46 @@
+package edu.ncsu.csc216.checkout_simulator.queues;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+import edu.ncsu.csc216.checkout_simulator.simulation.Log;
+
+/**
+ * Tests the functionality of the Store class.
+ * @author Brian Morris
+ *
+ */
+public class StoreTest {
+
+    /**
+     * Tests the Store class.
+     */
+    @Test
+    public void storeTest() {
+        // constructor
+        CheckoutRegister[] registers = new CheckoutRegister[5];
+        Log log = new Log();
+        registers[0] = new CheckoutRegister(log);
+        registers[1] = new CheckoutRegister(log);
+        registers[2] = new CheckoutRegister(log);
+        registers[3] = new CheckoutRegister(log);
+        registers[4] = new CheckoutRegister(log);
+        Store test = new Store(500, registers);
+        // size()
+        assertEquals(500, test.size());
+        // hasNext() true
+        assertTrue(test.hasNext());
+        // full queue departTimeNext()
+        assertTrue(test.departTimeNext() >= 0);
+        // Process all 500 carts.
+        for (int i = 0; i < 500; i++) {
+            assertTrue(test.processNext().isWaitingInRegisterLine());
+        }
+        // hasNext() false
+        assertFalse(test.hasNext());
+        // empty queue departTimeNext()
+        assertEquals(Integer.MAX_VALUE, test.departTimeNext());
+    }
+
+}
