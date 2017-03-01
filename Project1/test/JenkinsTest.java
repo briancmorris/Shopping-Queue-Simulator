efc0ae8b931311428c38d7d9ba03aa0aba60f4cb
Brian Morris <bcmorri3@ncsu.edu>
2017-02-28 19:35:26 -0500

    Added back test file to undo red ball on Jenkins.

---
+++ Project1/test/JenkinsTest.java
@@ -0,0 +1,18 @@
+import static org.junit.Assert.*;
+import org.junit.Test;
+
+/**
+ * Test class to get Jenkins working.
+ * @author Brian Morris
+ *
+ */
+public class JenkinsTest {
+
+    /**
+     * A test to get Jenkins working.
+     */
+    @Test
+    public void jenkinsTest() {
+        assertTrue(null == null);
+    }
+}
